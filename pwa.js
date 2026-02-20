f3bd20f971935469bbd2623ff1e2bce2947eeefc
tbaymon1-eng <tbaymon1@gmail.com>
2026-02-20 11:34:44 -0600

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -2,7 +2,21 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      await navigator.serviceWorker.register("./sw.js", { scope: "./" });
+      const reg = await navigator.serviceWorker.register("./sw.js", { scope: "./" });
+
+      // If there's an updated SW waiting, tell it to activate now
+      if (reg.waiting) reg.waiting.postMessage({ type: "SKIP_WAITING" });
+
+      // When a new SW is found, tell it to activate now
+      reg.addEventListener("updatefound", () => {
+        const newWorker = reg.installing;
+        if (!newWorker) return;
+        newWorker.addEventListener("statechange", () => {
+          if (newWorker.state === "installed" && navigator.serviceWorker.controller) {
+            newWorker.postMessage({ type: "SKIP_WAITING" });
+          }
+        });
+      });
     } catch (e) {
       // ignore
     }
